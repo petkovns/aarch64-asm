00cb34258ca2fbb6aa6975bc4adde20ace50a592
petko <petavns@gmail.com>
2019-04-04 02:18:27 +0200

    Second Commit

--- 01_basic_commands/main.s
+++ 01_basic_commands/main.s
@@ -1,15 +1,64 @@
-// first.s
+// main.s
+// Assembler directive - now come instructions of the program, can be also .data - now comes the data directive
 .text
- 
+
+// Assembler directive - main is global symbol, needed to start the program
 .globl main
- 
+
+// Entry point of program
 main:
-    add w0, w0, #2
-     
-    mov x0, #0                // x0 ← 0
-    mov x1, #0x1234           // x0 ← 0x1234
-    add x2, x0, x1, sxtw #1   // x2 ← x0 + (ExtendSigned16To64(x1) << 1)
-                            // this sets x2 to 0x2468
 
+// In aasm64 asm we have 32bit registers w0-w30 and 64bit registers x0-x30.
+// In w0 is stored number of program arguments (argc) 
+
+// Assign value, #0 is constant value integer (max 16bit), can be negative
+// mov reg, #n (w0 = 0;)
+    mov w0, #0
+// Assign hex value
+    mov w0, 0x1000
+
+// Add value, reg3 can be constant #1
+// add reg1, reg2, reg3 (reg1 = reg2 + reg3;) 
+    add w0, w0, #1
+
+// Multiplication values
+// mul reg1, reg2, reg3 (reg1 = reg2 * reg3;)
+    mul w0, w0, #2
+// umull + smull for excess bits (32 bit)
+// umullh + smullh (two 64 bit registers for 128 bit result)
+
+// Division of values
+// div reg1, reg2, reg3 (reg1 = reg2 / reg3;)
+    div w0, w0, #2
+// udiv + sdiv
+
+// Bitwise operations
+// NOT, mvn Rdest, Rsource (Rdest = ~Rsource;)
+    mvn w0, w0
+// AND, and Rdest, Rsource1, Rsource2  (Rdest = Rsource1 & Rsource2;)
+    and w0, w0, #1
+// OTHER BITWISE: OR = orr; EXCLUSIVE OR = eor, BIT CLEAR = bic, OR NOT = orn, EXCLUSIVE OR NOT = eon  
+
+// Shifting
+// Logical shift left (LSL), discard values bigger than register, add zeros to least significant n bits
+// reg, LSL, #n (reg<<n), reg*2^n
+    w0, LSL, #3
+// Logical shift right (LSR)
+    w0, LSR, #2
+// Arithmetic shift right (ASR), values in register are not discarted in shift process, but transfer from highest to lowest
+//  reg, ASR, #n
+    w0, ASR, #3
+
+// Extend value (Convert)
+// uxtb - Unsigned byte (8 bit);      ExtendUnsigned8ToXX(reg)
+// sxtb - Signed   byte (8 bit);      ExtendSigned8ToXX(reg)
+    add x0, x1, w2, sxtb
+    add w0, w1, w2, sxtb
+// uxth - Unsigned half word (16 bit) ExtendUnsigned16ToXX(reg)
+// sxth - Signed   half word (16 bit) ExtendSigned16ToXX(reg)
+// uxtw - Unsigned word (32 bit)      ExtendUnsigned32ToXX(reg)
+// sxtw - Signed   word (32 bit)      ExtendSigned32ToXX(reg)
+    add x0, x1, w2, sxtw
+
+// returns from main, finish program
     ret
-
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # aarch64-asm
 Easy sample codes in assemby for ARMv8 64bit. Testing with Raspberry Pi 3
+
+## Setup
+1. Install cross-compiler `sudo apt-get install qemu-user gcc-aarch64-linux-gnu`
+2. Install qemu version 3+
+3. Make examples by command `make all` and run in simulator by `make run`. 
